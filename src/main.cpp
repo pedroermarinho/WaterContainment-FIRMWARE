4071e5f71bbd975fbd618e349585e334fea2f992
Pedro Marinho <pedro.marinho@creathus.org.br>
2022-10-13 13:27:03 -0400

    melhorias

--- src/main.cpp
+++ src/main.cpp
@@ -6,22 +6,51 @@
 #include <PubSubClient.h>
 #include <Wire.h>
 
+#define SENSOR 27
+#define LED_ALARME 2
+#define LED_MQTT 4
+
 WiFiManager wifiManager; // Objeto de manipulação do wi-fi
 WiFiClient wifiClient;
 
 PubSubClient mqttClient(wifiClient);
 
+long currentMillis = 0;
+long previousMillis = 0;
+int interval = 1000;
+float calibrationFactor = 4.5;
+volatile byte pulseCount;
+byte pulse1Sec = 0;
+float flowRate;
+
+void IRAM_ATTR pulseCounter()
+{
+    pulseCount++;
+}
+
 void configModeCallback(WiFiManager *myWiFiManager);
 void saveConfigCallback();
 void mqttCallback(char *topic, byte *payload, unsigned int length);
 void reconnect();
-String getJson();
+String getJson(float value);
+void onAlarm();
 
 void setup()
 {
 
     Serial.begin(115200);
     Serial.println("...");
+
+    pinMode(LED_ALARME, OUTPUT);
+    pinMode(LED_MQTT, OUTPUT);
+
+    pinMode(SENSOR, INPUT_PULLUP);
+
+    pulseCount = 0;
+    flowRate = 0.0;
+    previousMillis = 0;
+
+    attachInterrupt(digitalPinToInterrupt(SENSOR), pulseCounter, FALLING);
 
     if (WiFi.status() != WL_CONNECTED)
     {
@@ -44,30 +73,47 @@ void setup()
     // callback para quando se conecta em uma rede, ou seja, quando passa a trabalhar em modo estação
     wifiManager.setSaveConfigCallback(saveConfigCallback);
 
-    mqttClient.setServer("192.168.0.17", 1883);
+    mqttClient.setServer("192.168.0.55", 1883);
+    mqttClient.setCallback(mqttCallback);
 
     Wire.begin();
-
 }
 
 int cont = 0;
 
 void loop()
 {
-
-    if (!mqttClient.connected())
+    currentMillis = millis();
+    if (currentMillis - previousMillis > interval)
     {
-        reconnect();
+        if (!mqttClient.connected())
+        {
+            reconnect();
+        }
+
+        pulse1Sec = pulseCount;
+        pulseCount = 0;
+
+        flowRate = ((1000.0 / (millis() - previousMillis)) * pulse1Sec) / calibrationFactor;
+        previousMillis = millis();
+
+        // Print the flow rate for this second in litres / minute
+        Serial.print("Flow rate: ");
+        Serial.print(int(flowRate)); // Print the integer part of the variable
+        Serial.print("L/min");
+        Serial.print("\n"); // Print tab space
+
+        mqttClient.loop();
+
+        if (int(flowRate) > 0)
+        {
+            mqttClient.publish("SENSORES", getJson(flowRate).c_str());
+        }
+
+        Serial.println(mqttClient.state());
+        Serial.println(mqttClient.connected());
     }
-
-    mqttClient.loop();
-    mqttClient.publish("SENSORES", getJson().c_str());
-
-    cont++;
-    Serial.println("cont: " + String(cont));
-    Serial.println(mqttClient.state());
-    Serial.println(mqttClient.connected());
-    delay(3000);
+    // delay(3000);
 }
 
 // callback que indica que o ESP entrou no modo AP
@@ -87,7 +133,27 @@ void saveConfigCallback()
 
 void mqttCallback(char *topic, byte *payload, unsigned int length)
 {
-    // message received
+    Serial.print("Message arrived on topic: ");
+    Serial.print(topic);
+    Serial.print(". Message: ");
+    String messageTemp;
+
+    for (int i = 0; i < length; i++)
+    {
+        messageTemp += (char)payload[i];
+    }
+
+    if (String(topic) == "ALARMES")
+    {
+        Serial.print("Changing output to ");
+        if (messageTemp == "on")
+        {
+            Serial.println("on");
+            onAlarm();
+        }
+    }
+
+    Serial.println(messageTemp);
 }
 
 void reconnect()
@@ -100,11 +166,13 @@ void reconnect()
         if (mqttClient.connect("teste", "watercontainment", "watercontainment123"))
         {
             Serial.println("connected");
+            digitalWrite(LED_MQTT, LOW);
             // Subscribe
-            mqttClient.subscribe("SENSORES");
+            mqttClient.subscribe("ALARMES");
         }
         else
         {
+            digitalWrite(LED_MQTT, HIGH);
             Serial.print("failed, rc=");
             Serial.print(mqttClient.state());
             Serial.println(" try again in 5 seconds");
@@ -114,13 +182,17 @@ void reconnect()
     }
 }
 
-String getJson()
+String getJson(float value)
 {
     String json = "{";
-    // json += "\"temperature\":\"" + String(temperature) + "\",";
-    // json += "\"luminosity\":\"" + String(luminosity) + "\",";
-    // json += "\"airHumidity\":\"" + String(airHumidity) + "\",";
-    // json += "\"soilHumidity\":\"" + String(soilHumidity) + "\"";
+    json += "\"litrosMin\":\"" + String(value) + "\"";
     json += "}";
     return json;
+}
+
+void onAlarm()
+{
+    digitalWrite(LED_ALARME, HIGH);
+    delay(5000);
+    digitalWrite(LED_ALARME, LOW);
 }
